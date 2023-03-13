a6a1695cb46443574dbc72fc3d4a390f3bc4aeda
triciahughes <hughes.a.tricia@gmail.com>
2023-03-13 14:33:55 -0700

    compleeted

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,9 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-primary')[0].contents)
+
+## completed ##
